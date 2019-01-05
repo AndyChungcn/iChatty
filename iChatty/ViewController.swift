ff9191b99243c9719f758c3c63a2bbac91efc70b
AndyChung <andychung1005@gmail.com>
2019-01-05 14:02:30 +0800

    initialize app

---
+++ iChatty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iChatty
+//
+//  Created by 钟汇杭 on 2019/1/5.
+//  Copyright © 2019 钟汇杭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
